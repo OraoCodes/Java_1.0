f11a68169932d8bfcdffe5d2c94e007283319849
Oraocodes <oraowycliffe@gmail.com>
2024-04-24 18:16:57 +0300

    introduce methods, multipy a method taking in two integers and outputting their multoplied versions

--- src/Main.java
+++ src/Main.java
@@ -4,52 +4,16 @@ import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-        //arrayList - practise
+        //introductions to Java Methods - a block of code one call at a particular time to perform a certain function.
 
-//        ArrayList<String> food = new ArrayList<String>();
-//
-//        food.add("Omena");
-//        food.add("fulu");
-//        food.add("kuon");
-//
-//        food.set(0, "susshi");
-//        food.remove(2);
-//        food.clear();
-//
-//        for (int i=0; i<food.size(); i++ ){
-//            System.out.println(food.get(i));
-//        }
-//
-//        //test 1
-//
-//        ArrayList<String> family = new ArrayList<>();
-//
-//        family.add("grandpa");
-//        family.add("grandma");
-//        family.add("mother");
-//
-//        for(int i=0; i<family.size(); i++){
-//            System.out.println(family.get(i));
-//        }
-//
-//        //testing out replacement of each word a with an i
-//
-//        ArrayList<Integer> current = new ArrayList<Integer>();
-//
-//        current.add(4);
-//        current.add(5);
-//
-//        for(int i : current){
-//            System.out.print(i*4);
-//        }
+        //testing out a method that takes in two integers and multiplies them
+        int x = 5;
+        int y = 10;
 
-        ArrayList<String> animals = new ArrayList<>();
-
-        animals.add("cat");
-        animals.add("bat");
-
-        for( String i : animals){
-            System.out.println(i.replace('a', 'i'));
-        }
+        System.out.println(multiply(x,y));
+    }
+    static int multiply(int a, int b){
+        int z = a * b;
+        return z;
     }
 }
